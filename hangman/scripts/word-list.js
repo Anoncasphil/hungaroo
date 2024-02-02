e1aa55971f2016e456c0896916adf146d798947b
Antoine Philipp Ochea <136882013+Anoncasphil@users.noreply.github.com>
2024-02-02 19:33:30 +0800

    Add files via upload

---
+++ hangman/scripts/word-list.js
@@ -0,0 +1,221 @@
+const wordList = [
+    {
+        word: "programming",
+        hint: "Creating computer programs."
+    },
+    {
+        word: "algorithm",
+        hint: "Step-by-step procedure for problem-solving in computing."
+    },
+    {
+        word: "database",
+        hint: "Organized data collection accessed electronically."
+    },
+    {
+        word: "encryption",
+        hint: "Converting data into code for security."
+    },
+    {
+        word: "server",
+        hint: "Provides resources or services over a network."
+    },
+    {
+        word: "cybersecurity",
+        hint: "Protecting computer systems from digital attacks."
+    },
+    {
+        word: "programming language",
+        hint: "Formal system for computer instruction."
+    },
+    {
+        word: "cloud computing",
+        hint: "Internet-based delivery of computing services."
+    },
+    {
+        word: "network",
+        hint: "Connected computers and devices for sharing resources."
+    },
+    {
+        word: "frontend",
+        hint: "User-interaction part of an application or website."
+    },
+    {
+        word: "backend",
+        hint: "Handles data processing and storage in a system."
+    },
+    {
+        word: "API",
+        hint: "Rules and tools for building software applications."
+    },
+    {
+        word: "bug",
+        hint: "Error, flaw, or unintended behavior in a program."
+    },
+    {
+        word: "debugging",
+        hint: "Finding and fixing errors in a computer program."
+    },
+    {
+        word: "code repository",
+        hint: "Place to store and manage source code."
+    },
+    {
+        word: "responsive design",
+        hint: "Designing for optimal viewing on various devices."
+    },
+    {
+        word: "API key",
+        hint: "Code identifying a program calling an API."
+    },
+    {
+        word: "agile development",
+        hint: "Flexible and collaborative software development."
+    },
+    {
+        word: "machine learning",
+        hint: "AI allowing computers to learn from experience."
+    },
+    {
+        word: "framework",
+        hint: "Pre-built code for efficient application development."
+    },
+    {
+        word: "virtualization",
+        hint: "Creating a virtual version, like virtual machines."
+    },
+    {
+        word: "IT support",
+        hint: "Assistance for technology-related issues."
+    },
+    {
+        word: "firewall",
+        hint: "Network security system controlling traffic."
+    },
+    {
+        word: "responsive web design",
+        hint: "Designing websites for various device views."
+    },
+    {
+        word: "user authentication",
+        hint: "Verifying a user's identity with credentials."
+    },
+    {
+        word: "data backup",
+        hint: "Creating copies to prevent data loss."
+    },
+    {
+        word: "IT infrastructure",
+        hint: "Hardware, software, networks enabling IT operations."
+    },
+    {
+        word: "DevOps",
+        hint: "Integrating software development and IT operations."
+    },
+    {
+        word: "API endpoint",
+        hint: "Specific URL for API operations."
+    },
+    {
+        word: "root access",
+        hint: "Highest level of control over a system."
+    },
+    {
+        word: "open source",
+        hint: "Software with publicly available source code."
+    },
+    {
+        word: "user interface",
+        hint: "Interaction point between user and program."
+    },
+    {
+        word: "API documentation",
+        hint: "Information to understand and use an API."
+    },
+    {
+        word: "scripting language",
+        hint: "Language for writing interpreted scripts."
+    },
+    {
+        word: "root cause analysis",
+        hint: "Identifying and addressing underlying issues."
+    },
+    {
+        word: "UX design",
+        hint: "Enhancing user satisfaction in product interaction."
+    },
+    {
+        word: "malware",
+        hint: "Software aiming to disrupt or damage systems."
+    },
+    {
+        word: "protocol",
+        hint: "Set of rules for communication between devices."
+    },
+    {
+        word: "data mining",
+        hint: "Analyzing large data sets for patterns and insights."
+    },
+    {
+        word: "cyber threat",
+        hint: "Potential malicious act against digital operations."
+    },
+    {
+        word: "data breach",
+        hint: "Unauthorized access leading to sensitive data exposure."
+    },
+    {
+        word: "IT governance",
+        hint: "Framework for effective use of IT resources."
+    },
+    {
+        word: "version control",
+        hint: "Managing changes to documents, code, or information."
+    },
+    {
+        word: "cloud security",
+        hint: "Measures to protect data and applications in the cloud."
+    },
+
+    {
+        word: "API Gateway",
+        hint: "A server that acts as an API front-end, receiving API requests, enforcing throttling and security policies, passing requests to the back-end service, and passing the response back to the requester."
+    },
+    {
+        word: "containerization",
+        hint: "A lightweight, executable software package that includes everything needed to run a piece of software, including the code, runtime, libraries, and system tools."
+    },
+    {
+        word: "load balancing",
+        hint: "The process of evenly distributing incoming network traffic across multiple servers to ensure no single server is overwhelmed, optimizing resource utilization and minimizing response time."
+    },
+    {
+        word: "microservices",
+        hint: "An architectural style that structures an application as a collection of small, independent services that can be developed, deployed, and scaled independently."
+    },
+    {
+        word: "SQL injection",
+        hint: "A type of attack where malicious SQL statements are inserted into an entry field for execution, often leading to unauthorized access to the database."
+    },
+    {
+        word: "hashing",
+        hint: "The process of converting input data (such as passwords) into a fixed-size string of characters, usually for security purposes."
+    },
+    {
+        word: "rootkit",
+        hint: "A type of malicious software designed to provide continued access to a computer while actively hiding its presence."
+    },
+    {
+        word: "scalability",
+        hint: "The capability of a system to handle a growing amount of work, or its potential to be enlarged to accommodate that growth."
+    },
+    {
+        word: "sandboxing",
+        hint: "A security mechanism that provides a restricted environment for executing untrusted or untested code, isolating it from the rest of the system."
+    },
+    {
+        word: "VPN",
+        hint: "Virtual Private Network - a secure connection between two or more devices or networks over the internet, providing privacy and security."
+    },
+];
+
+
